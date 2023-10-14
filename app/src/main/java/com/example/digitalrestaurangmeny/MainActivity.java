a200f11453417792a772714ee4b390e6d7c9ee9e
Juhee Kang <juhee.kang@infotiv.se>
2023-10-14 19:23:46 +0200

    wip-on going

--- app/src/main/java/com/example/digitalrestaurangmeny/MainActivity.java
+++ app/src/main/java/com/example/digitalrestaurangmeny/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.digitalrestaurangmeny;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView appetizerTextView, mainDishesTextView, dessertsTextView;
+    Button appetizersButton, mainDishesButton, dessertsButton;
 
-// test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    appetizersButton = findViewById(R.id.appetizers_button);
+    mainDishesButton = findViewById(R.id.main_dishes_button);
+    dessertsButton = findViewById(R.id.desserts_button);
+    appetizerTextView = findViewById(R.id.appetizers_view);
+    mainDishesTextView = findViewById(R.id.main_dishes_view);
+    dessertsTextView = findViewById(R.id.desserts_view);
+
+        appetizersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView();
+            }
+        });
+
     }
 }
